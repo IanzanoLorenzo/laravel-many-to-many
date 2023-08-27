7a1eab9502331ccb8d8803d6da711d71504e4e32
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-08-27 17:13:19 +0200

    modificato seeder progetto

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Project;
 use Faker\Generator as Faker;
 use Faker\Provider\en_US\Person;
+use Illuminate\Support\Str;
 
 
 class ProjectSeeder extends Seeder
@@ -20,8 +21,9 @@ class ProjectSeeder extends Seeder
     {
         for( $i=0; $i<10; $i++ ){
             $project = new Project;
+            $project->type_id = $faker->numberBetween(1, 5);
             $project->project_name = $faker->words(3, true);
-            $project->project_name_slug = $project->toSlug($project->project_name);
+            $project->project_name_slug = Str::slug($project->project_name);
             $project->description = $faker->text(100);
             $project->creator_name = $person->name();
             $project->creation_date = $faker->dateTimeBetween('-2 year', 'now');
